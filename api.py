39c865f80411fdb09a90df9888ad83c5f38d41dd
Girundi <43175525+Girundi@users.noreply.github.com>
2020-10-13 17:50:52 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,27 +1,51 @@
 from google.oauth2 import service_account
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 import io
 import pickle
 import requests
 import os
+import os.path
 import platform
+import mimetypes
+import base64
+from apiclient import errors
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 import pprint as pp
+import json
 import datetime
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = 'Emotions Project-481579272f6a.json'
+
+SCOPES = ['https://www.googleapis.com/auth/drive',
+          'https://www.googleapis.com/auth/gmail.send']
+SERVICE_ACCOUNT_FILE = 'Emotions_Project-481579272f6a.json'
+EMAIL_ACCOUNT_FILE = 'email_credentials.json'
 NVR_ACCOUNT_FILE = 'nvr_cred.json'
 nvr_server = 'https://nvr.miem.hse.ru/api/gdrive-upload'
 nvr_key = 'https://nvr.miem.hse.ru/api/gdrive-upload/504'
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+GOOGLE_CLIENT_ID = '332884163839-cgsk3ta79lgoo2o2otcb2h8ck28cd1if.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'bIb1mKNZH18LC5lSeDj1QTMk'
+
+EMAIL_FROM = "noreply@facerecognizer.com"
 
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# email_cred = service_account.Credentials.from_service_account_file(EMAIL_ACCOUNT_FILE, scopes=SCOPES)
 service = build('drive', 'v3', credentials=credentials)
+with open('Emotions_Project-481579272f6a.json', 'r') as j:
+    data = json.load(j)
+# delegated_credentials = credentials.with_subject(EMAIL_FROM)
+# service = build('gmail', 'v1', credentials=delegated_credentials)
 
 # credentials = service_account.Credentials.from_service_account_file(NVR_ACCOUNT_FILE, scopes=SCOPES)
 # service = build('drive', 'v3', credentials=credentials)
+
 
 def look_into_drive(dict_id, name_contains=None):
     if name_contains is None:
@@ -80,8 +104,7 @@ def upload_video(filename, upload_name, folder_id=None, room_num=None):
         media = MediaFileUpload(filename, resumable=True)
         r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
         print(r)
-        os.remove(filename)
-        return 0
+        return r
     elif room_num is not None:
         if platform.system() != "Windows":
             old_string = filename
@@ -94,7 +117,6 @@ def upload_video(filename, upload_name, folder_id=None, room_num=None):
         file = open(filename, 'rb')
         files = {'file': file}
         res = requests.post(nvr_server + "/" + room_num, files=files, headers=nvr_key)
-        os.remove(filename)
 
         return res.status_code
 
@@ -106,9 +128,118 @@ def edit_rooms(rooms, ids, tags):
     f.write(pickle.dumps(staff))
 
 
+def create_message(sender, to, subject, message_text, file=None):
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    if file is not None:
+        content_type, encoding = mimetypes.guess_type(file)
+
+        if content_type is None or encoding is not None:
+            content_type = 'application/octet-stream'
+        main_type, sub_type = content_type.split('/', 1)
+        if main_type == 'text':
+            fp = open(file, 'rb')
+            msg = MIMEText(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'image':
+            fp = open(file, 'rb')
+            msg = MIMEImage(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'audio':
+            fp = open(file, 'rb')
+            msg = MIMEAudio(fp.read(), _subtype=sub_type)
+            fp.close()
+        else:
+            fp = open(file, 'rb')
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(fp.read())
+            fp.close()
+        filename = os.path.basename(file)
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+
+def send_message(user_id, message):
+
+  """Send an email message.
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+  Returns:
+    Sent Message.
+  """
+  try:
+      service = get_service('token.pickle')
+      message = (service.users().messages().send(userId=user_id, body=message).execute())
+      print('Message Id: %s' % message['id'])
+      return message
+
+  except errors.HttpError as error:
+      print('An error occurred: %s' % error)
+
+
+def build_service():
+    """Shows basic usage of the Gmail API.
+        Lists the user's Gmail labels.
+        """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                f"{EMAIL_ACCOUNT_FILE}", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+
+def get_service(path):
+    with open(rf'{path}', 'rb') as token:
+        creds = pickle.load(token)
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+
+def send_file_with_email(to:str, subject:str, message_text, file=None):
+    # email_service = get_service('token.pickle')
+    message = create_message(EMAIL_FROM, to, subject, message_text, file)
+    send_message('me', message)
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+# send_file_with_email('iasizykh@miem.hse.ru', 'Test', 'Test')
+build_service()
+# message = create_message(EMAIL_FROM, 'iasizykh@miem.hse.ru', 'Test', 'Testment', 'queue/c9dc7d2c-1508-4676-b703-1bd7d2ab9d86.mp4')
+# send_message('me', message)
 # pp = pprint.PrettyPrinter(indent=4)
 
-
+# r = upload_video("video_output/twice.mp4", "twice.mp4", folder_id="14Xsw4xk6vUFINsyy1OH5937Rq98W4JHw")
+# print(r)
 # now = datetime.datetime.now()
 # today = now.strftime('%Y-%m-%d')
 #
--- app.py
+++ app.py
@@ -3,32 +3,90 @@ import os
 import uuid
 import yaml
 from werkzeug.utils import secure_filename
-
+import api
+from celery import Celery
 from celery.result import AsyncResult
 from celery.utils.log import get_task_logger
-from flask import Flask, redirect, render_template, request, send_from_directory
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 from flask_celery import make_celery
 from rofl import ROFL
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
+import sqlite3
+from db import init_db_command
+from user import User
+import asyncio
+import redis
+from OpenSSL import SSL, crypto
+from self_sign_cert import gen_self_signed_cert
+
+
+cert_dir = 'certificates'
+CERT_FILE = 'certificate.crt'
+KEY_FILE = 'app.key'
+
+# context = SSL.Context(SSL.SSLv23_METHOD)
+# cert, key = gen_self_signed_cert()
+# open(os.path.join(cert_dir, CERT_FILE), "wt").write(cert)
+# open(os.path.join(cert_dir, KEY_FILE), "wt").write(key)
+# context.use_privatekey_file(os.path.join(cert_dir, KEY_FILE))
+# context.use_certificate_file(os.path.join(cert_dir, CERT_FILE))
+
 
 config_path = os.path.abspath(os.path.join(os.getcwd(), "config.yml"))
-config = yaml.load(open(config_path))
+config = yaml.load(open(config_path), Loader=yaml.FullLoader)
+rofl_folder = "14Xsw4xk6vUFINsyy1OH5937Rq98W4JHw"
+GOOGLE_CLIENT_ID = api.GOOGLE_CLIENT_ID
+GOOGLE_CLIENT_SECRET = api.GOOGLE_CLIENT_SECRET
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 app = Flask(__name__)
-app.config.update(config)
+app.config.update(result_backend='redis://127.0.0.1:6379/0', broker_url='redis://127.0.0.1:6379/0')
 
-rofl = ROFL("trained_knn_model.clf", retina=True, on_gpu=True, emotions=True)
+rofl = ROFL("trained_knn_model.clf", retina=True, on_gpu=False, emotions=True)
 
-celery = make_celery(app)
+celery = Celery(main=__name__, broker='redis://127.0.0.1:6379/0', backend='redis://127.0.0.1:6379/0')
+
+# celery = make_celery(app)
 
 logger = logging.getLogger(__name__)
 celery_logger = get_task_logger(__name__)
 
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-file_handler = logging.FileHandler(app.config['LOGFILE'])
+file_handler = logging.FileHandler(config['LOGFILE'])
 file_handler.setFormatter(formatter)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
+
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+ioloop = asyncio.get_event_loop()
+
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 def set_logger(logger):
@@ -46,12 +104,86 @@ celery_logger = set_logger(celery_logger)
 @app.route('/')
 def index():
     """Start page."""
-    return render_template('index.html')
+    print(request.remote_addr)
+
+    displayment = 'none'
+    if current_user.is_authenticated:
+        displayment = 'inline'
+        user = current_user.name
+        return render_template('upload.html', displayment=displayment, username=user)
+    return render_template('index.html', displayment=displayment)
+
+
+@app.route("/login")
+def login():
+    google_provider_cfg = api.get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    google_provider_cfg = api.get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 
 def allowed_file(filename):
     """Check format of the file."""
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/upload', methods=['POST'])
@@ -63,6 +195,9 @@ def upload():
             logger.error(msg)
             return render_template('exception.html', text=msg)
 
+        emotions = "emotions" in request.form
+        recognize = "recognize" in request.form
+        remember = "remember" in request.form
         file = request.files['file']
         if file and not allowed_file(file.filename):
             msg = f'the file {file.filename} has wrong extention'
@@ -70,43 +205,82 @@ def upload():
             return render_template('exception.html', text=msg)
 
         path = os.path.abspath(os.path.join(
-            os.getcwd(), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+            os.getcwd(), config['UPLOAD_FOLDER'], secure_filename(file.filename)))
         filename, file_extension = os.path.splitext(path)
 
         # Set the uploaded file a uuid name
         filename_uuid = str(uuid.uuid4()) + file_extension
-        path_uuid = os.path.abspath(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'], filename_uuid))
+        path_uuid = os.path.abspath(os.path.join(os.getcwd(), config['UPLOAD_FOLDER'], filename_uuid))
 
         file.save(path_uuid)
         logger.info(f'the file {file.filename} has been successfully saved as {filename_uuid}')
-        return redirect('/process/' + filename_uuid)
+        # processing.apply_async((filename_uuid, emotions, recognize, remember), link_error=error_handler.s())
+        processing(filename_uuid, emotions, recognize, remember)
+        return redirect('/')
 
 
-@app.route('/process/<filename>')
-def task_processing(filename):
+@app.route('/thank')
+def thank():
     """Process the image endpoint."""
-    task = processing(filename)
+
     # async_result = AsyncResult(id=task.task_id, app=celery)
     # processing_result = async_result.get()
-    return render_template('result.html', video_name=task)
+
+    return render_template('thank.html')
 
 
-@app.route('/result/<filename>')
 def send_file(filename):
+    r = api.upload_video("video_output/" + filename, filename.split('/')[-1], folder_id=rofl_folder)
+    _id = r['id']
     """Show result endpoint."""
-    return send_from_directory(os.path.abspath(os.path.join(os.getcwd(), app.config['RESULT_FOLDER'])),
-                               filename)
+    return "https://drive.google.com/file/d/" + _id + "/preview"
+
+
+# async def run(filename, fps_factor, recog, remem, em):
+#     await ioloop.run_in_executor(None, rofl.basic_run, "queue", filename, fps_factor, recog, remem, em)
 
 
 @celery.task(name='celery.processing')
-def processing(filename):
+def processing(filename, em=True, recog=True, remem=True):
     """Celery function for the image processing."""
+
+    # rofl = ROFL("trained_knn_model.clf", retina=True, on_gpu=False, emotions=True)
+
     celery_logger.info(f'{filename} is processing')
 
-    rofl.basic_run("queue", filename, emotions=True, fps_factor=30)
-    celery_logger.info(f'processing {filename} is completed')
+    # ioloop.run_until_complete(run(filename, 30, recog, remem, em))
+    rofl.basic_run("queue", filename, fps_factor=30, recognize=recog, remember=remem, emotions=em)
+    celery_logger.info(f'processing {filename} is finished')
+
+    i = 30
+    while not os.path.isfile("video_output/" + filename) and i != 0:
+        import time
+        time.sleep(1)
+        i -= 1
+    api.send_file_with_email(current_user.email, "Processed video",
+                             "Thank you, that's your processed video",
+                             "video_output/" + filename)
+    os.remove("queue/" + filename)
+    r = api.upload_video("video_output/" + filename, filename.split('/')[-1], folder_id=rofl_folder)
+    _id = r['id']
+
     return filename
 
 
+@celery.task
+def error_handler(uuid):
+    result = AsyncResult(uuid)
+    exc = result.get(propagate=False)
+    print('Task {0} raised exception: {1!r}\n{2!r}'.format(
+          uuid, exc, result.traceback))
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # exec('celery -A app.celery worker --loglevel=info')
+    # celery.worker_main()
+    # task = processing.apply_async(('twice.mp4', True, False, False), ignore_result=True)
+    # print(task)
+    # print(celery.current_worker_task)
+    # result = AsyncResult(id=task.task_id, app=celery).get()
+    context = (os.path.join(cert_dir, CERT_FILE), os.path.join(cert_dir, KEY_FILE))
+    app.run( ssl_context=context, debug=True, threaded=True, port='8080')
