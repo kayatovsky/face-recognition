652cd0761f42ddca36ec0f151665a96046f19db0
kayatovsky <57445347+kayatovsky@users.noreply.github.com>
2020-11-19 21:21:58 +0300

    Add files via upload

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,25 +2,26 @@ import os
 from kombu import Queue, Exchange
 REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
-broker_url = os.environ.get(
+BROKER_URL = os.environ.get(
     'REDIS_URL', "redis://{host}:{port}/2".format(
         host=REDIS_HOST, port=str(REDIS_PORT)))
-result_backend = broker_url
-
-worker_max_tasks_per_child = 4
-task_queues = (
+CELERY_RESULT_BACKEND = BROKER_URL
+CELERYD_MAX_TASKS_PER_CHILD = 1
+CELERYD_PREFETCH_MULTIPLIER = 1
+CELERY_ACKS_LATE = True
+CELERY_QUEUES = (
     Queue('high', Exchange('high'), routing_key='high'),
     Queue('normal', Exchange('normal'), routing_key='normal'),
     Queue('low', Exchange('low'), routing_key='low'),
 )
 
-task_default_queue = 'normal'
-task_default_exchange = 'normal'
-task_default_routing_key = 'normal'
+CELERY_DEFAULT_QUEUE = 'normal'
+CELERY_DEFAULT_EXCHANGE = 'normal'
+CELERY_DEFAULT_ROUTING_KEY = 'normal'
 
-task_routes = {
+CELERY_ROUTES = {
     # -- HIGH PRIORITY QUEUE -- #
-    'app.celery.processing': {'queue': 'high'},
+    'celery.processing': {'queue': 'high'},
     # -- LOW PRIORITY QUEUE -- #
-    'app.celery.processing_nvr': {'queue': 'low'},
+    'celery.processing_nvr_': {'queue': 'low'},
 }
