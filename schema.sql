0ccb552874cb5d11a654657f5ef16ab30eeb6d20
Girundi <43175525+Girundi@users.noreply.github.com>
2020-12-27 18:45:34 +0300

    Добавлена таблица для записей

--- schema.sql
+++ schema.sql
@@ -4,3 +4,24 @@ CREATE TABLE user (
   email TEXT UNIQUE NOT NULL,
   profile_pic TEXT NOT NULL
 );
+
+CREATE TABLE recording (
+  file TEXT PRIMARY KEY,
+  room_num TEXT NOT NULL,
+  rec_date TEXT NOT NULL,
+  rec_time TEXT NOT NULL,
+  json TEXT NOT NULL
+);
+
+--CREATE TABLE room (
+--  room_num TEXT PRIMARY KEY,
+--  google_folder TEXT UNIQUE NOT NULL,
+--  tag TEXT NOT NULL
+--);
+--
+--CREATE TABLE face (
+--  id TEXT PRIMARY KEY,
+--  encoding TEXT NOT NULL,
+--  name TEXT
+--  first_pic TEXT NOT NULL
+--);
--- user.py
+++ user.py
@@ -33,3 +33,38 @@ class User(UserMixin):
             (id_, name, email, profile_pic),
         )
         db.commit()
+
+
+class Recording:
+    def __init__(self, file, room_num, rec_date, rec_time, json):
+        self.file = file
+        self.room = room_num
+        self.rec_date = rec_date
+        self.rec_time = rec_time
+        self.json = json
+
+    @staticmethod
+    def get(room_num, rec_date, rec_time):
+        db = get_db()
+        recording = db.execute(
+            "SELECT * FROM recording WHERE room = ? AND rec_date = ? AND rec_time = ?", (room_num, rec_date, rec_time)
+        ).fetchone()
+        if not recording:
+            return None
+
+        recording = Recording(
+            file=recording[0], room_num=recording[1], rec_date=recording[2], rec_time=recording[3], json=recording[4]
+        )
+        return recording
+
+    @staticmethod
+    def create(file, room_num, rec_date, rec_time, json):
+        db = get_db()
+        db.execute(
+            "INSERT INTO recording (file, room_num, rec_date, rec_time, json) "
+            "VALUES (?, ?, ?, ?, ?)",
+            (file, room_num, rec_date, rec_time, json),
+        )
+        db.commit()
+
+
